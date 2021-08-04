fbf85360e3b7aa1be39110ef85713a889aeed1f7
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-08-04 23:03:18 +0530

    added styles to display output

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ submitBtn.addEventListener("click", function(event){
 
         let profitPercent = ((profit/purchasePriceValue) * 100).toFixed(2);
 
+        output.style.border = "2px";
+        output.style.background = "linear-gradient(to right, #00D956, #0468CD)";
+        output.style.boxShadow = "rgba(0, 0, 0, 0.4) 0px 30px 90px";
+       
+
         output.innerHTML =`You have gained ${profitPercent}% .You have made a total profit of ₹${profit}`;
 
         stocksBackground.style.backgroundImage = "url('./images/celebration1_gif.gif')";
@@ -37,6 +42,8 @@ submitBtn.addEventListener("click", function(event){
 
         let lossPercent = ((loss/purchasePriceValue) * 100).toFixed(2);
 
+        output.style.border = "2px solid black";
+
         output.innerHTML =`You lost ${lossPercent}%. Your total loss is ₹${loss}`;
 
         if(lossPercent > 50){
@@ -45,6 +52,11 @@ submitBtn.addEventListener("click", function(event){
             stocksBackground.style.backgroundRepeat = "no-repeat";
 
             stocksBackground.style.backgroundSize = "cover";
+
+            output.style.border = "2px solid white";
+
+            output.innerHTML =`You lost ${lossPercent}%. Your total loss is ₹${loss}`;
+
         }
     }
 
